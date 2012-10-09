5314c5661e2d4a6de7e4c3c52dc75292262182d8
inhell <hahazhi@gmail.com>
2012-10-09 16:07:16 +0800

    wzb

--- kan/crontab/spider/spider_index.php
+++ kan/crontab/spider/spider_index.php
@@ -18,36 +18,56 @@ include dirname(__FILE__).'/../../libs/spiderlib.php';
 // 	$model->create($row);
 // }
 
-$youku_url = "http://www.youku.com/v_olist/c_96_a__s__g__r__lg__im__st__mt__tg__d_1_et_0_fv_0_fl__fc__fe__o_7_p_{page}.html";
+// $youku_url = "http://www.youku.com/v_olist/c_96_a__s__g__r__lg__im__st__mt__tg__d_1_et_0_fv_0_fl__fc__fe__o_7_p_{page}.html";
+
+// for($page = 1; $page<=30 ;$page++)
+// {
+// 	$pagetr = array("{page}"=>$page);
+// 	$targeturl = strtr($youku_url, $pagetr);
+
+// 	$row['title'] = "优酷电影分页".$page;
+// 	$row['siteid'] = 2;
+// 	$row['spidercall'] = 1;
+	
+// 	$row['vtype'] = 1;
+// 	$row['targeturl'] = $targeturl;
+// 	echo $targeturl."...over\r\n";
+// 	$model = mini_db_model::model('vspiders');
+// 	$model->create($row);
+// }
+// mini_db_unitofwork::getHandle()->commit();
+// $qq_url="http://v.qq.com/list/1_-1_-1_-1_1_0_{page}_20_0_-1.html";
+// $tudou_url = "http://movie.tudou.com/albumtop/c22t-1v-1z-1a-1y-1h-1s1p{page}.html";
+
+// for($page = 1; $page<=1 ;$page++)
+// {
+// 	$pagetr = array("{page}"=>$page);
+// 	$targeturl = strtr($tudou_url, $pagetr);
+	
+// 	$row['title'] = "土豆电影分页".$page;
+// 	$row['siteid'] = 1;
+// 	$row['spidercall'] = 1;
+// 	$row['vtype'] = 1;
+// 	$row['targeturl'] = $targeturl;
+// 	echo $targeturl."...over\r\n";
+// 	$model = mini_db_model::model('vspiders');
+// 	$model->create($row);
+// }
+// mini_db_unitofwork::getHandle()->commit();
+
+
+$qq_url="http://v.qq.com/list/1_-1_-1_-1_1_0_{page}_20_0_-1.html";
+
 
 for($page = 1; $page<=30 ;$page++)
 {
 	$pagetr = array("{page}"=>$page);
-	$targeturl = strtr($youku_url, $pagetr);
+	$targeturl = strtr($qq_url, $pagetr);
 
-	$row['title'] = "优酷电影分页".$page;
-	$row['siteid'] = 2;
-	$row['spidercall'] = 1;
-	
-	$row['vtype'] = 1;
-	$row['targeturl'] = $targeturl;
-	echo $targeturl."...over\r\n";
-	$model = mini_db_model::model('vspiders');
-	$model->create($row);
-}
-mini_db_unitofwork::getHandle()->commit();
-
-$tudou_url = "http://movie.tudou.com/albumtop/c22t-1v-1z-1a-1y-1h-1s1p{page}.html";
-
-for($page = 1; $page<=1 ;$page++)
-{
-	$pagetr = array("{page}"=>$page);
-	$targeturl = strtr($tudou_url, $pagetr);
-	
-	$row['title'] = "土豆电影分页".$page;
+	$row['title'] = "QQ电影分页".$page;
 	$row['siteid'] = 1;
 	$row['spidercall'] = 1;
-	$row['vtype'] = 1;
+	$row['vtype'] = 9;
 	$row['targeturl'] = $targeturl;
 	echo $targeturl."...over\r\n";
 	$model = mini_db_model::model('vspiders');
