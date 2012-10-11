694be8e8d54912279a2b89f57eb14d0ba9cea743
inhell <hahazhi@gmail.com>
2012-10-11 15:29:20 +0800

    wzb

--- kan/crontab/spider/test.php
+++ kan/crontab/spider/test.php
@@ -5,4 +5,18 @@ include dirname(__FILE__).'/../init.php';
 $params = array('cookiefile'=>'./test.txt');
 		
 $curl = new mini_tool_curl($params);
-print_r($curl->getData("http://movie.douban.com/subject_search?cat=1002&search_text=潮爆大状"));
+$bid="";
+for($i=0;$i<100;$i++){
+$params = array('cookiefile'=>'./test.txt');
+	$curl->getData("http://movie.douban.com/subject_search?cat=1002&search_text=潮爆大状");
+	$cookie = file_get_contents($params['cookiefile']);
+	
+	if(preg_match('/bid.*?"(.*?)"/ism', $cookie,$match))
+	{
+		$bid .= $match[1]."\t";
+	}
+	file_put_contents($params['cookiefile'], '');
+	echo "clear...\r\n";
+}
+file_put_contents("./bid.data",$bid);
+
