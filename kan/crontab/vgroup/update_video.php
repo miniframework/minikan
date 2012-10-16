a5988b6e11743f1eb52e51f60d4b11f4622b1db8
inhell <hahazhi@gmail.com>
2012-10-16 22:33:06 +0800

    wzb

--- kan/crontab/vgroup/update_video.php
+++ kan/crontab/vgroup/update_video.php
@@ -2,5 +2,5 @@
 include dirname(__FILE__).'/../init.php';
 
 $spider = new spiderService();
-$spider->updateEpisodeVideo("where   epsign <>2 limit 0,10");
+$spider->updateEpisodeVideo("where  epsign <>2");
 ?>
