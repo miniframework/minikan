bd08028e4a05e35e4c03b015097e50883d25c975
inhell <hahazhi@gmail.com>
2012-10-17 16:30:18 +0800

    wzb

--- kan/views/layout/kan_main.view.php
+++ kan/views/layout/kan_main.view.php
@@ -18,5 +18,18 @@
 	<span>Copyright &copy; 2012 .com. All Rights Reserved. </span> 
 	<em><a href="#" target="_blank">京ICP备11002035号</a></em>
 </div>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-21332676-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 </body>
 </html>
