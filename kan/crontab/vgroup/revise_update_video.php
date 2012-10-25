f0079bd96f1233780e770960b15ca9c72dcec2c0
inhell <hahazhi@gmail.com>
2012-10-25 23:25:40 +0800

    wzb

--- kan/crontab/vgroup/revise_update_video.php
+++ kan/crontab/vgroup/revise_update_video.php
@@ -2,5 +2,5 @@
 include dirname(__FILE__).'/../init.php';
 
 $spider = new spiderService();
-$spider->updateEpisodeVideo("where  status =2");
+$spider->updateEpisodeVideo("where  status =2 and vgroupid !=0");
 ?>
