895032ddf79acc98cf599a9bf9b33997a5904ec7
inhell <hahazhi@gmail.com>
2012-10-26 11:50:54 +0800

    wzb

--- webroot/index.php
+++ webroot/index.php
@@ -3,17 +3,19 @@
  * @author mini
  * @data	
  */
-
-date_default_timezone_set('Asia/Shanghai');
 /**
  * define app HOME path
  */
-
 defined('RUNPATH') || define('RUNPATH', dirname(__FILE__)."/../kan");
 defined('MINI_EXCEPTION_HANDLER') || define('MINI_EXCEPTION_HANDLER',true);
 defined('MINI_ERROR_HANDLER') || define('MINI_ERROR_HANDLER',true);
 defined('MINI_DEBUG') || define('MINI_DEBUG', true);
-if($_SERVER['REMOTE_ADDR'] == '72.14.189.178') header("Location:http://www.ailezi.com".$_SERVER['REQUEST_URI']);
+
+/**
+ * include bootstrap  file
+ */
+include RUNPATH."/config/bootstrap.php";
+
 /**
  * include php miniframework import file
  */
