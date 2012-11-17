2a320a3241c110402cc07da927a633f55b9df602
inhell <hahazhi@gmail.com>
2012-11-17 20:39:01 +0800

    wzb

--- kan/crontab/vgroup/update_tv.php
+++ kan/crontab/vgroup/update_tv.php
@@ -2,5 +2,5 @@
 include dirname(__FILE__).'/../init.php';
 
 $spider = new spiderService();
-$spider->updateEpisodeVideo("where vtype=2");
+$spider->updateEpisodeVideo("where vtype=2 and id >46168");
 ?>
