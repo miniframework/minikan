fb26ace39f47dcebad2c4b38d4793285c0dcb287
inhell <hahazhi@gmail.com>
2012-11-17 23:24:21 +0800

    wzb

--- kan/crontab/vgroup/update_comic.php
+++ kan/crontab/vgroup/update_comic.php
@@ -2,5 +2,5 @@
 include dirname(__FILE__).'/../init.php';
 
 $spider = new spiderService();
-$spider->updateEpisodeVideo("where vtype=3 and id>68325 order by id");
+$spider->updateEpisodeVideo("where vtype=3 and id>79095 order by id");
 ?>
